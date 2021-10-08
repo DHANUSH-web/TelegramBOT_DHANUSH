3dd5321a58e4dcf75094f09adfc95c0ecf232dc8
DHANUSH H V <67680433+DHANUSH-web@users.noreply.github.com>
2021-10-08 20:22:16 +0530

    Update constants.py

--- TelegramBOT/constants.py
+++ TelegramBOT/constants.py
@@ -1,1 +1,8 @@
-TELEGRAM_API_TOKEN = "2068317385:AAECv4W5ym5EcgME-hymyPfUr5eiIj9DuSc"
+'''
+How to get API Token:
+>>> Search for BotFather in Telegram and click start
+>>> Inside BotFather, type /newbot and hit enter
+>>> Follow the instructions and copy the TOKEN and paste here
+'''
+
+TELEGRAM_API_TOKEN = "paste your telegram api token here"
